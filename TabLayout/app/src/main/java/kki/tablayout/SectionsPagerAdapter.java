05ed3799caea61c4a0ac68a28b7514f3a6127f90
Ikhofua <kikhofua2@gmail.com>
2016-07-26 18:38:11 -0400

    TabLayout

---
+++ TabLayout/app/src/main/java/kki/tablayout/SectionsPagerAdapter.java
@@ -0,0 +1,47 @@
+package kki.tablayout;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by 207493 on 7/26/16.
+ */
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+    public Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext =context;
+    }
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page
+        // Return a PlaceholderFragment (defined as a static inner class below)
+        return MainActivity.PlaceholderFragment.newInstance(position + 1);
+    }
+    @Override
+    public int getCount() {
+        // Show 3 total pages
+        return 3;
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "My Tasks";
+            case 1:
+                return "Leaderboard";
+            case 3:
+                return "Friends";
+        }
+        return null;
+    }
+}
